b24af72b00de2b61d79b397e141382fc456ffb70
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-05-18 12:06:05 +0200

    Cache

--- edge-service/src/main/java/com/thomasvitale/edgeservice/multitenancy/security/LoginController.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/multitenancy/security/LoginController.java
@@ -9,7 +9,6 @@ import org.springframework.security.web.server.ServerRedirectStrategy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class LoginController {
