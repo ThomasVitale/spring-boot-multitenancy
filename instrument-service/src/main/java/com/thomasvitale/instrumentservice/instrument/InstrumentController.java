a4a06fea0fb948f86b69bb668033ed61799a7934
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-05-18 12:08:18 +0200

    Cache start

--- instrument-service/src/main/java/com/thomasvitale/instrumentservice/instrument/InstrumentController.java
+++ instrument-service/src/main/java/com/thomasvitale/instrumentservice/instrument/InstrumentController.java
@@ -31,7 +31,7 @@ public class InstrumentController {
   	}
 
 	@GetMapping("{type}")
-	@Cacheable(cacheNames = "instrumentTypes", keyGenerator = "tenantKeyGenerator")
+	@Cacheable(cacheNames = "instrumentTypes")
 	List<Instrument> getInstrumentByType(@PathVariable String type) {
     	log.info("Returning instrument of type: {}", type);
     	return instrumentRepository.findByType(type);
