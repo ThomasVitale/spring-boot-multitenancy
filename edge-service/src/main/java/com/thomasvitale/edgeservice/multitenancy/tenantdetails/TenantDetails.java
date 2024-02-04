01272fec86aaf04fd0462dbc7a95e39817bf67a6
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-02-04 10:36:28 +0100

    04-gateway-end

--- edge-service/src/main/java/com/thomasvitale/edgeservice/multitenancy/tenantdetails/TenantDetails.java
+++
@@ -1,9 +0,0 @@
-package com.thomasvitale.edgeservice.multitenancy.tenantdetails;
-
-public record TenantDetails(
-        String identifier,
-        boolean enabled,
-        String clientId,
-        String clientSecret,
-        String issuer
-) {}
