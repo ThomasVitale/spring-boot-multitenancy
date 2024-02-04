f80f668dac23d610d0f03456b99d36f9e82fa4f0
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-02-04 10:57:56 +0100

    01-tenant-begin

--- instrument-service/src/main/java/com/thomasvitale/instrumentservice/multitenancy/web/TenantContextFilter.java
+++ instrument-service/src/main/java/com/thomasvitale/instrumentservice/multitenancy/web/TenantContextFilter.java
@@ -7,49 +7,20 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import com.thomasvitale.instrumentservice.multitenancy.context.TenantContextHolder;
-import com.thomasvitale.instrumentservice.multitenancy.context.resolvers.HttpHeaderTenantResolver;
-import com.thomasvitale.instrumentservice.multitenancy.exceptions.TenantNotFoundException;
-import com.thomasvitale.instrumentservice.multitenancy.exceptions.TenantResolutionException;
-import com.thomasvitale.instrumentservice.multitenancy.tenantdetails.TenantDetailsService;
-
-import io.micrometer.common.KeyValue;
-
-import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.filter.ServerHttpObservationFilter;
 
 @Component
 public class TenantContextFilter extends OncePerRequestFilter {
 
-	private final HttpHeaderTenantResolver httpRequestTenantResolver;
-
-	public TenantContextFilter(HttpHeaderTenantResolver httpHeaderTenantResolver) {
-		this.httpRequestTenantResolver = httpHeaderTenantResolver;
-    }
-
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var tenantIdentifier = httpRequestTenantResolver.resolveTenantIdentifier(request);
-
-        if (StringUtils.hasText(tenantIdentifier)) {
-            TenantContextHolder.setTenantIdentifier(tenantIdentifier);
-        } else {
-            throw new TenantResolutionException("A valid tenant must be specified for requests to %s".formatted(request.getRequestURI()));
-        }
-
-        try {
-            filterChain.doFilter(request, response);
-        } finally {
-            TenantContextHolder.clear();
-        }
+        filterChain.doFilter(request, response);
 	}
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
-        return request.getRequestURI().startsWith("/actuator");
+        return false;
     }
 
 }
