9b9dabe8b153a373dee0e178537205696397febd
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-02-05 15:27:03 +0100

    Update

--- instrument-service/src/main/java/com/thomasvitale/instrumentservice/multitenancy/context/resolvers/TenantResolver.java
+++ instrument-service/src/main/java/com/thomasvitale/instrumentservice/multitenancy/context/resolvers/TenantResolver.java
@@ -5,9 +5,6 @@ import org.springframework.lang.Nullable;
 @FunctionalInterface
 public interface TenantResolver<T> {
 
-    /**
-     * Resolves a tenant identifier from the given source.
-     */
     @Nullable
     String resolveTenantIdentifier(T source);
 
